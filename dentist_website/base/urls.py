f3a7e5b21e8111c2e311c1427b5cdc93b9eb01f4
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2022-03-16 20:50:07 -0500

    update(start about.html)

--- dentist_website/base/urls.py
+++ dentist_website/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns= [
     path ('', views.home),
-    path ('home.html', views.home)
+    path ('home.html', views.home),
+    path ('about.html', views.about)
 ]
--- dentist_website/base/views.py
+++ dentist_website/base/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+def about(request):
+    return render(request, 'base/about.html')

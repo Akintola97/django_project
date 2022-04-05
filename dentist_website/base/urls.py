2c68aa389a5f37f5794b06b21ec5d1001cfc5d28
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2022-04-05 18:44:06 -0500

    update

--- dentist_website/base/urls.py
+++ dentist_website/base/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns= [
     path ('', views.home),
-    path ('home.html', views.home),
-    path ('about.html', views.about),
+    path ('home.html', views.home, name='home'),
+    path ('about.html', views.about, name= 'about'),
     path ('contact.html', views.contact, name= 'contact'),
-    path ('appointment.html', views.appointment),
-    path ('services.html', views.services),
+    path ('appointment.html', views.appointment, name= 'appointment'),
+    path ('services.html', views.services, name = 'services'),
   
 ]

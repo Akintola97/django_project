e539a67314ada34dbe7ae1ec448ba9aa71508b87
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2022-04-09 19:48:04 -0500

    update

---
+++ dentist_website/dentist_website/staticfiles/js/app.js
@@ -0,0 +1,10 @@
+ var updateButton = document.getElementsByClassName('addcart-btn');
+
+ for (var i = 0; i < updateButton.length; i++ ){
+    updateButton[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+    })
+} 
+

096587b53919ae0f21c702689bdeaec9a6b3e07d
Akintola97 <akinoyetayo97@gmail.com>
2022-05-22 21:54:23 -0500

    update from django_bootstrap

--- dentist_website/base/views.py
+++ dentist_website/base/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
 # Create your views here.
 
 def home(request):
@@ -7,25 +6,6 @@ def home(request):
 def about(request):
     return render(request, 'base/about.html',{})
 def contact (request):
-   if request.method == 'POST':
-      patron_name = request.POST['patron-name'] 
-      patron_email = request.POST['patron-email']
-      patron_subject = request.POST['patron-subject']
-      patron_message = request.POST['patron-message']
-     
-                    
-                    
-                  #send an emai
-      #send_mail(
-       # patron_name, #name
-        #patron_subject, # subject
-        #patron_message, #message
-        #patron_email, #from email
-        #['akinoyetayo@rocketmail.com'], #To Email
-        #)
-      #return render(request, 'contact.html',{'patron_name':patron_name})
-  
-   #else:
    return render(request, 'base/contact.html')
  
 def appointment (request):
--- dentist_website/dentist_website/urls.py
+++ dentist_website/dentist_website/urls.py
@@ -1,4 +1,4 @@
-"""dentist_website URL Configuration
+"""django_bootstrap URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/

55838ba872081c8c79bcd90b8b12d9195c8f965d
Akintola97 <akinoyetayo97@gmail.com>
2022-05-25 00:30:09 -0500

    Update prod.py

--- dentist_website/dentist_website/settings/prod.py
+++ dentist_website/dentist_website/settings/prod.py
@@ -11,6 +11,39 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    ''
+    'boltdental-website.herokuapp.com'
 ]
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version':1,
+    'disable_existing_loggers': False,
+    'formatters':{
+    
+    'verbose':{
+        'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s %(message)s",
+        'datefmt': "%d/%b/%Y %H:%M:%S"
+    },
+    'simple': {
+        'format':'%(levelname)s %(messages)s'
+        },
+    },
+    'handlers':{
+        'console':{
+            'level':'DEBUG',
+            'class':'loggin.StreamHandler',
+        },
+    },
+    'loggers':{
+        'MYAPP':{
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+    }
+}
+
+
+#heroku settings 
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
